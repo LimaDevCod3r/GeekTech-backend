41235c1ebca107e519d1a6ed04c013b1549e494b
Diego Lima <diegolima20100@gmail.com>
2026-04-25 10:07:27 -0300

    chore: initialize Spring Boot project

---
+++ src/main/java/com/diegolima/geekTech/GeekTechApplication.java
@@ -0,0 +1,13 @@
+package com.diegolima.geekTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diegolima/geekTech/GeekTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegolima.geekTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
